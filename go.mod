dcae419088254a5ae87281b77397c5daa3a93d4f
Sergio Ugalde <serch037@gmail.com>
2020-09-05 03:02:09 -0500

    Reorg

--- go.mod
+++ go.mod
@@ -4,11 +4,13 @@ go 1.15
 
 require (
 	github.com/99designs/gqlgen v0.12.2
+	github.com/PaesslerAG/gval v1.0.1 // indirect
 	github.com/agnivade/levenshtein v1.1.0 // indirect
 	github.com/dgryski/trifles v0.0.0-20200830180326-aaf60a07f6a3 // indirect
 	github.com/facebook/ent v0.4.2
 	github.com/go-chi/chi v3.3.2+incompatible
 	github.com/go-chi/jwtauth v4.0.4+incompatible
+	github.com/go-chi/render v1.0.1
 	github.com/go-redis/redis/v8 v8.0.0-beta.9
 	github.com/google/go-cmp v0.5.2 // indirect
 	github.com/google/uuid v1.1.2 // indirect
@@ -20,7 +22,12 @@ require (
 	github.com/lestrrat/go-pdebug v0.0.0-20180220043741-569c97477ae8 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/onsi/ginkgo v1.14.0 // indirect
+	github.com/ory/dockertest/v3 v3.6.0
 	github.com/rs/cors v1.6.0
+	github.com/rs/zerolog v1.15.0
+	github.com/steinfletcher/apitest v1.4.10
+	github.com/steinfletcher/apitest-jsonpath v1.5.1
+	github.com/stretchr/testify v1.6.1
 	github.com/vektah/gqlparser/v2 v2.0.1
 	golang.org/x/exp v0.0.0-20200901203048-c4f52b2c50aa // indirect
 	golang.org/x/net v0.0.0-20200822124328-c89045814202 // indirect
--- main.go
+++ main.go
@@ -1,134 +1,34 @@
 package main
 
 import (
-	"context"
-	"database/sql"
 	"log"
-	"net/http"
 	"os"
-	"time"
 
-	"github.com/go-chi/jwtauth"
+	"foodworks.ml/m/internal/api"
 
-	"foodworks.ml/m/auth"
-	"foodworks.ml/m/internal/generated/ent"
-	generated "foodworks.ml/m/internal/generated/graphql"
-	"foodworks.ml/m/internal/resolver"
-	"github.com/99designs/gqlgen/graphql"
-	"github.com/99designs/gqlgen/graphql/handler"
-	"github.com/99designs/gqlgen/graphql/handler/extension"
-	"github.com/99designs/gqlgen/graphql/handler/lru"
-	"github.com/99designs/gqlgen/graphql/handler/transport"
-	"github.com/99designs/gqlgen/graphql/playground"
-	"github.com/facebook/ent/dialect"
-	entsql "github.com/facebook/ent/dialect/sql"
-	"github.com/go-chi/chi"
-	"github.com/go-redis/redis/v8"
-	"github.com/gorilla/websocket"
-	_ "github.com/jackc/pgx/v4/stdlib"
-	"github.com/rs/cors"
+	"foodworks.ml/m/internal/platform"
 )
 
-func Server(es graphql.ExecutableSchema) *handler.Server {
-	srv := handler.New(es)
-	srv.AddTransport(&transport.Websocket{
-		Upgrader: websocket.Upgrader{
-			CheckOrigin: func(r *http.Request) bool {
-				return true
-			},
-			ReadBufferSize:  1024,
-			WriteBufferSize: 1024,
-		},
-		KeepAlivePingInterval: 15 * time.Second,
-	})
-
-	srv.AddTransport(transport.Options{})
-	srv.AddTransport(transport.GET{})
-	srv.AddTransport(transport.POST{})
-	srv.AddTransport(transport.MultipartForm{})
-
-	srv.SetQueryCache(lru.New(1000))
-
-	srv.Use(extension.Introspection{})
-	srv.Use(extension.AutomaticPersistedQuery{
-		Cache: lru.New(100),
-	})
-
-	return srv
-}
-
-// Open new db connection
-func Open(databaseURL string) (*sql.DB, *ent.Client) {
-	db, err := sql.Open("pgx", databaseURL)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Create an ent.Driver from `db`.
-	drv := entsql.OpenDB(dialect.Postgres, db)
-	return db, ent.NewClient(ent.Driver(drv))
-}
-
 func main() {
-	// init db client
-	db, client := Open(os.Getenv("POSTGRES_URL"))
+	platform.SetupLog()
 
-	defer client.Close()
-	defer db.Close()
-	// Run the auto migration tool.
-	if err := client.Schema.Create(context.Background()); err != nil {
-		log.Printf("failed creating schema resources: %v", err)
-		return
+	dataStoreConfig := platform.DataStoreConfig{
+		os.Getenv("POSTGRES_URL"),
+		os.Getenv("REDIS_ADDR"),
+		os.Getenv("REDIS_PASS"),
 	}
-	// init redis
-	rdb := redis.NewClient(&redis.Options{
-		Addr:     os.Getenv("REDIS_ADDR"),
-		Password: os.Getenv("REDIS_PASS"), // no password set
-		DB:       0,                       // use default DB
-	})
 
-	// init server
-	router := chi.NewRouter()
-	// Add CORS middleware around every request
-	// See https://github.com/rs/cors for full option listing
-	router.Use(cors.New(cors.Options{
-		// AllowedOrigins:   []string{"*"},
-		AllowCredentials: true,
-		// Debug:            true,
-		AllowOriginFunc: func(origin string) bool {
-			return true
-		},
-	}).Handler)
-	router.Use(jwtauth.Verifier(auth.TokenAuth))
-	router.Use(auth.Middleware())
+	db, client := platform.NewEntClient(dataStoreConfig)
 
-	srv := Server(generated.NewExecutableSchema(generated.Config{Resolvers: &resolver.Resolver{Client: client, Redis: rdb}}))
-	appPort := os.Getenv("APPLICATION_PORT")
+	rdb := platform.NewRedisClient(dataStoreConfig)
 
-	router.Handle("/graphql/playground", playground.Handler("GraphQL playground", "/graphql"))
-	router.Handle("/graphql", srv)
-	router.Post("/auth", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hi")
-		// A very simple health check.
-		w.WriteHeader(http.StatusOK)
-		w.Header().Set("Content-Type", "application/json")
-	})
-	router.Post("/.api/auth", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hi")
-		// A very simple health check.
-		w.WriteHeader(http.StatusOK)
-		w.Header().Set("Content-Type", "application/json")
-	})
-	router.Post("/*", func(w http.ResponseWriter, r *http.Request) {
-		log.Println("Hi")
-		// A very simple health check.
-		w.WriteHeader(http.StatusOK)
-		w.Header().Set("Content-Type", "application/json")
-	})
+	api := api.API{}
+	api.SetupRoutes(client, rdb)
+	api.StartServer()
 
-	log.Printf("connect to http://localhost:%s/graphql/playground for GraphQL playground", appPort)
-	err := http.ListenAndServe(appPort, router)
-	if err != nil {
-		panic(err)
-	}
+	// Cleanup
+	log.Println("Closing")
+	db.Close()
+	rdb.Close()
+	client.Close()
 }
